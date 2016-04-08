350d93c03e1a1a2019cba3a205a109d9055560f5
rlreed <rlreed@ksu.edu>
2016-04-08 16:55:29 -0500

    Updated comments as well as some values

--- flux_spectrum.py
+++ flux_spectrum.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from scipy import constants
 from scipy.integrate import quad
-from scipy.optimize import minimize
 from matplotlib import rc
 rc('font',**{'family':'serif'})
 from matplotlib import rcParams
@@ -17,81 +17,86 @@ rcParams.update({'figure.autolayout': True})
 # Boltzmann constant in eV/K
 k = constants.value('Boltzmann constant in eV/K')
 
-
-
-
 class Flux():
     """
     This class evaluate neutron flux at energy e. 
-    At thermal energy range (e < 1eV), the flux is approximated by Maxwellian 
+    At thermal energy range (e < e1 eV), the flux is approximated by Maxwellian 
     distribution (D&H book Eq.(9-6)). 
-    At fast energy range (0.1MeV < e < 20MeV), the flux is approximated by U-235 
+    At fast energy range (e2 MeV < e < 20MeV), the flux is approximated by U-235 
     chi-spectrum (D&H book Eq.(2-112)).
-    At epithermal energies (1eV < e < 0.1MeV), flux = 1/e
+    At epithermal energies (e1 eV < e < e2 MeV), flux = 1/e
     
-    
-    r : thermal-to-fast flux ratio
+    ratio : thermal-to-fast flux ratio
     """
-    def __init__(self, r = 2, thermal_t = 600.0):
-        self.e2 = 1e5
-        self.thermal_t = thermal_t
+    def __init__(self, ratio = 2, thermal_temp = 600.0):
+        self.e2 = 1e6
+        self.thermal_temp = thermal_temp
         
         # Maxwellian distribution, Eq.(9-6)
-        self.m = lambda x : x ** 0.5 * np.exp(-x / (k * self.thermal_t))
+        self.m = lambda x : x ** 0.5 * np.exp(-x / (k * self.thermal_temp))
 
         # U235 chi distribution, Eq.(2-112)
         self.chi = lambda x : np.exp(-1.036e-6 * x) * np.sinh((2.29e-6 * x) ** 0.5)     
                          
         # Middle energy range
         self.f = lambda x : 1 / x
-        self.r = 0
-        E = np.logspace(-5,1,1000)
-        R = np.array([self.balance(e) for e in E])
-        plt.loglog(E, R)
-        plt.xlabel('Energy cutoff [eV]')
-        plt.ylabel('thermal to non-thermal flux ratio, r')
+        
+        # Compute ratio as a function of thermal cutoff
+        E = np.logspace(-4, 0.1, 200)
+        R = np.array([self.compute_ratio(e1) for e1 in E])
+        
+        # Create plot of ratio vs cutoff energy
+        plt.loglog(R, E)
+        plt.ylabel('Thermal Energy Cutoff [eV]')
+        plt.xlabel('thermal to non-thermal flux ratio, r')
         plt.savefig('ratio_to_cutoff.pdf')
         plt.clf()
-        self.e1 = np.interp(r, R, E)
+        
+        # Compute thermal cutoff for given ratio
+        self.e1 = np.interp(ratio, R, E)
         print 'Thermal cutoff is {} eV'.format(self.e1)
-        self.r = r
+        self.ratio = ratio
+        
+        # Compute constants for each part of the spectrum
         self.c1 = 1.0
         self.c2 = self.m(self.e1) / self.f(self.e1)
         self.c3 = self.c2 * self.f(self.e2) / self.chi(self.e2)
         
-    def balance(self, x):
-        A = quad(self.m, 0, x)[0]
-        B = self.m(x) / self.f(x) * quad(self.f, x, self.e2)[0]
-        C = self.m(x) / self.f(x) * self.f(self.e2) / self.chi(self.e2) * quad(self.chi, self.e2, 2e7)[0]
-        Q = A / (B + C) - self.r
-        return abs(Q)
-        
+    def compute_ratio(self, e1):
+        A = quad(self.m, 0, e1)[0]
+        C2 = self.m(e1) / self.f(e1)
+        C3 = self.f(self.e2) / self.chi(self.e2)
+        B = C2 * quad(self.f, e1, self.e2)[0]
+        C = C2 * C3 * quad(self.chi, self.e2, 2e7)[0]
+        r = A / (B + C)
+        return r
         
     def compute_flux(self, e):
-        """
-        Evaluate flux at Energy e.
-        e : neutron energy in eV
-        """
+        # Evaluate flux at Energy e in eV
         # thermal
-        if e <= self.e1:
-            return self.c1 * self.m(e)
-        # epithermal
-        elif self.e1 < e <= self.e2:
-            return self.c2 / e
-        elif e >= self.e2:
-            return self.c3 * self.chi(e)
+        return self.c1 * self.m(e) if e <= self.e1 else (self.c2 / e if e <= self.e2 else self.c3 * self.chi(e))
 
 # ratio of thermal flux to fast flux = 1e-5
 # neutron temperature = 600K
-f = Flux(2, 600.0)
-e = np.logspace(-2, 7, 1000)
-flux = np.zeros(len(e))
-for i in range(len(e)):
-    # f.compute_flux can return flux at a specific energy e (eV).
-    flux[i] = f.compute_flux(e[i])
+f = Flux(1e-5, 600.0)
+E = np.logspace(-5, 7, 1000)
+flux = np.zeros(E.shape)
+
+# Compute the flux at all energies
+for i, e in enumerate(E):
+    flux[i] = f.compute_flux(e)
+lethergy = E * flux
     
-plt.loglog(e, flux)
+# Plot the flux
+plt.loglog(E, flux)
 plt.xlabel('E (eV)')
 plt.ylabel('flux')
-plt.savefig('flux_spectrum')
+plt.savefig('flux_spectrum.pdf')
+plt.clf()
+
+# Plot the lethergy
+plt.loglog(E, lethergy)
+plt.xlabel('E (eV)')
+plt.ylabel('$E\phi(E)$')
+plt.savefig('lethergy.pdf')
     
