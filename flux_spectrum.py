bfb141eb5b360e9f6918010db91a2cad4bc1d949
rlreed <rlreed@ksu.edu>
2016-04-13 14:56:59 -0500

    Remove unused import

--- flux_spectrum.py
+++ flux_spectrum.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from scipy import constants
 from scipy.integrate import quad
 from matplotlib import rc
